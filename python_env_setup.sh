6ef11ea31e1f37de6efeb1c20954ffacdfd5c64a
Jude <sauve031@umn.edu>
2019-09-11 10:39:57 -0500

    Update python_setup.sh - Updated PS1 Mod for git
    There was an extra space at the beginning of git tag for PS1. This might not belong here, but no other place atm.

--- python_env_setup.sh
+++ python_env_setup.sh
@@ -157,7 +157,7 @@ EXCL
 cat <<EXCL >> $RC_FILE
 # Custom solution to disp git branch, from webs
 parse_git_branch() {
-     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
 PS1='$(parse_git_branch)\[\033[00m\]'$PS1
 
