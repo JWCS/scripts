38d6b3bb03ff685c3ad77cdb441d2048064dab7e
Jude <sauve031@umn.edu>
2019-11-12 11:05:10 -0600

    python_env_setup: Fixing .bashrc cannot source
    .bashrc cannot source from script because non-interactive. 
    https://unix.stackexchange.com/questions/481816/sourcing-bashrc-inside-script-doesnt-update-the-env-variables
    Making temporary file to source, then adding to bashrc at

--- python_env_setup.sh
+++ python_env_setup.sh
@@ -3,13 +3,13 @@
 # User should read up on the following to know how to use it.
 # DOCS: See each section:
 # AUTHOR: Jude Sauve <sauve031@umn.edu>
-# VERSION 1.0 : LAST_CHANGED 2019-09-05
+# VERSION 1.1 : LAST_CHANGED 2019-11-12
 # LICENSE: MIT
 
 # How to develop with python once this is all installed
 # In your new project directory, make a .envrc file
 # In your ~/.direnvrc file, at the bottom is some commented-out code, for a .envrc file
-# Place the code in the .envrc file. The double commented out lines should still be commented out. 
+# Place the code in the .envrc file. The double commented out lines should still be commented out.
 # I usually copy-paste another .envrc file when I make a new project
 # You need to change two things: the `pyversion=X.Y.Z` needs the version of python you want to use.
 # The `pvenv=PROJECT-NAME` needs the project name.
@@ -17,12 +17,12 @@
 # Afterwards, whenever you cd into this directory, you're in a special python environment, separate
 # from every other environment. If you cd out, you will leave the environment.
 # Try to only install with pip, BUT: do not use --user/-H and do not use sudo.
-# If you have not used this before, your version of python you want to use will not be installed. 
+# If you have not used this before, your version of python you want to use will not be installed.
 # To install a python version, use `pyenv install X.Y.Z` (this will auto-complete).
 # For example, to install the last 3.5 version, run `pyenv install 3.5.7` - or use more recent version.
 # There should be words on the left of the terminal interface, with possibly your python environment
 # or git branch. These were also done in here; it's called the PS1. To change this, change it in your
-# ~/.bashrc file. If it doesn't work, make an issue. 
+# ~/.bashrc file. If it doesn't work, make an issue.
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   RC_FILE=~/.bashrc
@@ -43,10 +43,14 @@ echo "You should delete ALL python stuff under ~/.local/ and delete all 'site_pa
 # https://stackoverflow.com/questions/1885525/how-do-i-prompt-a-user-for-confirmation-in-bash-script
 read -p "Would you like to continue? (have you deleted stuff?) " -n 1 -r
 echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+  [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
 fi
+
+# Cannot source .bashrc in script, .bashrc exits w/o running
+# Making tmp file to store additions, then adding to RC_FILE
+TMP_FILE=~/temp_file_delete_if_seen.sh
+touch $TMP_FILE
 
 # virtualenv
   # DOCS: https://virtualenv.pypa.io/en/latest/
@@ -54,18 +58,21 @@ fi
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   sudo apt-get install python-pip
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-  python get-pip.py
-  rm get-pip.py
+  curl https://bootstrap.pypa.io/get-pip.py -o ~/get-pip.py
+  python ~/get-pip.py
+  rm ~/get-pip.py
 fi
 pip install --user virtualenv
 
-cat <<EXCL >> $RC_FILE
+cat <<EXCL >> $TMP_FILE
 
 # Custom Additions
 
 EXCL
 
+cat <<EXCL >> $TMP_FILE
+# pyenv setup
+EXCL
 # pyenv + pyenv-virtualenv plugin
   # DOCS: https://objectpartners.com/2018/11/27/managing-multiple-python-instances/
   # DOCS: https://github.com/pyenv/pyenv/blob/master/COMMANDS.md
@@ -78,22 +85,28 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
   libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
   xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev libffi-dev \
   liblzma-dev python-openssl git
-  curl https://pyenv.run | bash
-  echo 'export PYENV_ROOT="$HOME/.pyenv"'>> $RC_FILE
-  echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> $RC_FILE
+  if [ -d ~/.pyenv ]; then
+    echo ""
+    echo "WARNING: pyenv already installed? Directory in ~/.pyenv, not re-installing."
+    echo "Check ~/.bashrc for duplicate code"
+    echo ""
+  else
+    curl https://pyenv.run | bash
+  fi
+  echo 'export PYENV_ROOT="$HOME/.pyenv"'>> $TMP_FILE
+  echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> $TMP_FILE
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   brew update
   brew install curl git
   brew install pyenv pyenv-virtualenv
 fi
-cat <<EXCL >> $RC_FILE
-# pyenv setup
+cat <<EXCL >> $TMP_FILE
 eval "\$(pyenv init -)"
 eval "\$(pyenv virtualenv-init -)"
 
 EXCL
-  
-source $RC_FILE
+
+source $TMP_FILE
 pyenv update
 
 # direnv: see github wiki for below configs; do last
@@ -103,7 +116,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   brew install direnv
 fi
-cat <<EXCL >> $RC_FILE
+cat <<EXCL >> $TMP_FILE
 # direnv config
 eval "\$(direnv hook bash)"
 
@@ -111,6 +124,11 @@ EXCL
 # direnv - python
   # DOCS: https://github.com/direnv/direnv/wiki/Python
   # Can get .envrc sample from .direnvrc file
+if [ -f ~/.direnv ]; then
+  echo ""
+  echo "WARNING: file ~/.direnv already exists. Check that there are no duplications"
+  echo ""
+fi
 cat <<EXCL >> ~/.direnvrc
 use_python() {
     if [ -n "\$(which pyenv)" ]; then
@@ -158,6 +176,11 @@ EXCL
   # DOCS: https://github.com/direnv/direnv/wiki/Docker-Machine
 # direnv - git:
   # DOCS: https://github.com/direnv/direnv/wiki/Git
+if [ -f ~/.gitignore_global ]; then
+  echo ""
+  echo "WARNING: file ~/.gitignore_global already exists. Check that there are no duplications"
+  echo ""
+fi
 git config --global core.excludesfile "~/.gitignore_global"
 cat <<EXCL >> ~/.gitignore_global
 # Direnv stuff
@@ -169,7 +192,7 @@ EXCL
   # All generated file exclusions to project .gitignore
 # direnv - tmux
   # DOCS: https://github.com/direnv/direnv/wiki/Tmux
-cat <<EXCL >> $RC_FILE
+cat <<EXCL >> $TMP_FILE
 alias tmux='direnv exec / tmux'
 
 EXCL
@@ -177,7 +200,7 @@ EXCL
   # DOCS: https://github.com/direnv/direnv/wiki/PS1
   # DOCS: https://github.com/direnv/direnv/wiki/Python
   # direnv needs to come last in bashrc, but ps1 can have more mods (git!)
-cat <<EXCL >> $RC_FILE
+cat <<EXCL >> $TMP_FILE
 # Custom solution to disp git branch, from webs
 parse_git_branch() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
@@ -195,7 +218,13 @@ PS1='\$(show_virtual_env)'\$PS1
 
 EXCL
 
-source $RC_FILE
+source $TMP_FILE
+cat $TMP_FILE >> $RC_FILE
+rm $TMP_FILE
+
+echo ""
+echo ""
+echo "FURTHER INSTRUCTIONS:"
 echo "You should clean up / order your .bashrc, don't let it get messy"
 echo "To install python, look up 'pyenv', or enter it in the terminal, 'pyenv versions'"
 echo "Install a python version using 'pyenv install VERSION' Or use -l to list options"
